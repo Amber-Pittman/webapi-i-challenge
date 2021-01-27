43c2d9f624d592a966f8aaccea937044dfd11bcc
Amber-Pittman <amber@amberpittman.com>
2021-01-26 21:17:41 -0500

    Updated dependencies

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hey all y\'all!')
+});
+
+server.get('/what', (req, res) => {
+    res.send('What you want, Earl?')
+});
+
+server.listen(8000, () => console.log('API running on port 8000'));
