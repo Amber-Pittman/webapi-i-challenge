d07c8523f23b786d5da462fa85825921d25b6d50
Amber-Pittman <amber@amberpittman.com>
2021-01-27 21:59:19 -0500

    Added hobbit data

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // implement your API here
+
+// import express into the file
 const express = require('express');
 
+// create the server
 const server = express();
 
 server.get('/', (req, res) => {
@@ -11,4 +14,21 @@ server.get('/what', (req, res) => {
     res.send('What you want, Earl?')
 });
 
+server.get('/hobbits', (req, res) => {
+    const hobbits = [
+        {
+            id: 1,
+            name: 'Samwise Gamgee',
+        },
+        {
+            id: 2,
+            name: 'Frodo Baggins',
+        },
+    ]
+
+    // set headers
+    res.status(200).json(hobbits)
+})
+
+// get server to listen for people looking for our specific server
 server.listen(8000, () => console.log('API running on port 8000'));
